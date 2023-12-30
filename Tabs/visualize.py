c1b7d641043f3d7264dbc0243ab9afcbf37c211a
ardinursahwal <148542995+ardinursahwal@users.noreply.github.com>
2023-12-30 17:12:23 +0700

    Update visualize.py

--- Tabs/visualize.py
+++ Tabs/visualize.py
@@ -5,17 +5,18 @@ from sklearn import tree
 import streamlit as st
 from web_function import train_model
 
+from sklearn.model_selection import train_test_split
+
 def app(df, x, y):
   warnings.filterwarnings("ignore")
   st.set_option('deprecation.showPyplotGlobalUse', False)
-
   st.title("Visualisasi Data")
-
+  x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=101)
   if st.checkbox("Plot Confusion Matrix"):
     model, score = train_model(x,y)
     plt.figure(figsize=(10,10))
-    pred = model.predict(x)
-    cm = confusion_matrix(y, pred, labels=model.classes_)
+    pred = model.predict(x_test)
+    cm = confusion_matrix(y_test, pred, labels=model.classes_)
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
     disp.plot()
     st.pyplot()
